aeaf31afce1f7c64ee836a66bbd01c49afecb435
Marek Pala <marek.pala@wincor-nixdorf.com>
2014-12-05 15:24:11 +0100

    RCP: double click listener

--- com.opcoach.restaurant.rcp/src/com/opcoach/restaurant/rcp/ApplicationWorkbenchWindowAdvisor.java
+++ com.opcoach.restaurant.rcp/src/com/opcoach/restaurant/rcp/ApplicationWorkbenchWindowAdvisor.java
@@ -1,6 +1,5 @@
 package com.opcoach.restaurant.rcp;
 
-import org.eclipse.swt.graphics.Point;
 import org.eclipse.ui.application.ActionBarAdvisor;
 import org.eclipse.ui.application.IActionBarConfigurer;
 import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
@@ -18,7 +17,6 @@ public class ApplicationWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor {
     
     public void preWindowOpen() {
         IWorkbenchWindowConfigurer configurer = getWindowConfigurer();
-        configurer.setInitialSize(new Point(400, 300));
         configurer.setShowCoolBar(false);
         configurer.setShowStatusLine(false);
     }
